0d66cfe43276bb9c2fa2c1353e0cf5004d1635aa
Dxlol <vovayarmoli1234@mail.ru>
2026-02-14 14:04:50 +0300

    j

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 import sys
-eyeCascade = cv2.CascadeClassifier(cascPath)
+eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 cam = cv2.VideoCapture(0)
 
 while True:
@@ -9,9 +9,12 @@ while True:
     roi = frame[269:795, 200:600]
     gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     
-    eyes = c
-
-    _, threshold = cv2.threshold(gray_roi, 5, 255, cv2.THRESH_BINARY_INV)
+    eyes = eyeCascade.detectMultiScale(gray_roi, 1.3, 5)
+    for (ex, ey, ew, eh) in eyes:
+        eye_roi = gray_roi[ey:ey+eh, ex:ex+ew]
+        _, threshold = cv2.threshold(eye_roi, 70, 255, cv2.THRESH_BINARY)
+    
+    # contur = cv2.findContours(threshold, cv2)
 
     cv2.imshow('frame', threshold)
     key = cv2.waitKey(30)
