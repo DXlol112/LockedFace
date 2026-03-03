d0a4b7c0d0ae682d8a659293285b1b2f1c07b6ec
DxLol <vovayarmolik1234@mai.ru>
2026-03-03 17:06:47 +0300

    ...

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ import time
 def time_input(time_user):
     pass
 
-
-
-
-
 def base_program():
     cap = cv2.VideoCapture(0)
     face_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -23,26 +19,24 @@ def base_program():
 
         if not ret:
             print("404")
-            exit()
+            break
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         #лицо
-        face = face_cascade_db.detectMultiScale(gray, 1.1, 19)
-        for (x,y,w,h) in face:
+        face = face_cascade_db.detectMultiScale(gray, 1.1, 19,minSize=(100, 100))
+        if len(face) > 0:
+            (x, y, w, h) = face[0]
             cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-
-            if len(face) == 0:
-                print('не видно')
 
             #глаза
             gray_fase = gray[y:y+h,x:x+w]
-            eyes = eye_cascade_db.detectMultiScale(gray_fase, 1.1, 19)
+            eyes = eye_cascade_db.detectMultiScale(gray_fase, 1.1, 19, minSize=(40, 40))
             for (ex, ey, ew, eh) in eyes:
                 cv2.rectangle(frame, (x+ex, y+ey),(x+ex + ew, y+ey + eh), (255,0,0), 2)
 
-                if len(eyes) == 0:
-                    print("глаз не видно")
+                if len(eyes) < 2:
+                    print("Глаз не видно")
 
 
         cv2.imshow('frame', frame)
